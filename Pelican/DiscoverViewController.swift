357250dad6bb899c2703100555d482bcc3f1fd58
Jack Swiggett <jackswiggett@gmail.com>
2016-12-01 17:21:44 -0800

    Implement sidebar menu navigation between four screens, using the SWRevealViewController library

---
+++ Pelican/DiscoverViewController.swift
@@ -0,0 +1,47 @@
+//
+//  DiscoverViewController.swift
+//  Pelican
+//
+//  Created by Jack Swiggett on 12/1/16.
+//  Copyright © 2016 Pelitech. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverViewController: UIViewController {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Configure menu button to show navigation menu
+        if let revealViewController = self.revealViewController() {
+            menuButton.target = revealViewController
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+            revealViewController.rearViewRevealWidth = 150
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pelican/FriendsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  FriendsViewController.swift
+//  Pelican
+//
+//  Created by Jack Swiggett on 12/1/16.
+//  Copyright © 2016 Pelitech. All rights reserved.
+//
+
+import UIKit
+
+class FriendsViewController: UIViewController {
+
+    // MARK: Properties
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Configure menu button to show navigation menu
+        if let revealViewController = self.revealViewController() {
+            menuButton.target = revealViewController
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
